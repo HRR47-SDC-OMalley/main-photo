b50001e988c9073992405a500a0e47135d8f8ce2
Steven Walker <swalk001@gmail.com>
2020-08-05 23:28:20 -0400

    implemented database with S3 urls

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+const db = require('../db/index.js')
 
 app.use(express.static(__dirname + '/../public/dist'))
+
+db.generateDB();
+
 
 app.listen(port, function () {
  console.log('Listening ' + port);
