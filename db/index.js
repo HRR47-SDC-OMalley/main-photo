693440cfd0931a20e183bc57dcc26b00da3bb31c
Steven Walker <swalk001@gmail.com>
2020-08-07 17:36:14 -0400

    eslint_config_setup

--- db/index.js
+++ db/index.js
@@ -13,15 +13,15 @@ let imagesSchema = mongoose.Schema({
 let Images = mongoose.model('Images', imagesSchema);
 
 let dropDatabase =  () => mongoose.connection.dropDatabase();
-//new
-// let retrieveListingImages = (listingId, callback) => {
-//   let constURL = "https://acoustic-guitar-images.s3.us-east-2.amazonaws.com/";
-//   Images.find({listing_id: listingId}).exec((err, results) => {
-//     if (err) {return err}
-//     callback(results);
-//   });
-// }
+
+let retrieveListingImages = (listingId, callback) => {
+  let constURL = "https://acoustic-guitar-images.s3.us-east-2.amazonaws.com/";
+  Images.find({listing_id: listingId}).exec((err, results) => {
+    if (err) {return err}
+    callback(results);
+  });
+}
 
 module.exports.Images = Images;
 module.exports.dropDatabase = dropDatabase;
-// module.exports.retrieveListingImages = retrieveListingImages;
+module.exports.retrieveListingImages = retrieveListingImages;
